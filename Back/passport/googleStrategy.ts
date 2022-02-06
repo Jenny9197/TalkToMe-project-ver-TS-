2b689ad1c28b6d5c649fe61183ff995b1801ecd1
Jenny9197 <passionnhope0110@gmail.com>
2022-02-06 17:02:32 +0900

    [fix]

--- Back/passport/googleStrategy.ts
+++ Back/passport/googleStrategy.ts
@@ -1,24 +1,21 @@
 import passport from "passport";
-import GoogleStrategy from 'passport-google-oauth2';
-import { User } from '../models';
+import GoogleStrategy from "passport-google-oauth2";
+import { User } from "../models";
 
-function getRandomInt(min:number, max:number) { //min ~ max 사이의 임의의 정수 반환
+function getRandomInt(min: number, max: number) {
+  //min ~ max 사이의 임의의 정수 반환
   return Math.floor(Math.random() * (max - min)) + min;
 }
 module.exports = () => {
   passport.use(
     new GoogleStrategy.Strategy(
       {
-        clientID: process.env.GOOGLE_CLIENT_ID||"test",
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET||"test",
-        callbackURL: 'http://ozam.shop/user/google/callback',
+        clientID: process.env.GOOGLE_CLIENT_ID || "test",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET || "test",
+        callbackURL: "http://ozam.shop/user/google/callback",
       },
-    //   function (accessToken, refreshToken, profile, cb) {
-    //     User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //       return cb(err, user);
-    //     });
-    //   }
-    async (accessToken, refreshToken, profile, done) => {
+
+      async (accessToken, refreshToken, profile, done) => {
         try {
           const exUser = await User.findOne({
             where: { snsId: profile.id, provider: "google" },
@@ -29,7 +26,7 @@ module.exports = () => {
             console.log(profile.name);
             const newUser = await User.create({
               email: profile._json && profile._json.email,
-              nickname: '아무개'+ getRandomInt(1,10000),
+              nickname: "아무개" + getRandomInt(1, 10000),
               // nickname: profile.displayName,
               snsId: profile.id,
               provider: "google",
