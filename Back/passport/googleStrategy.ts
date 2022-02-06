80fd4e9035951d9b172ab07bf39a6fac31743a23
Jenny9197 <passionnhope0110@gmail.com>
2022-02-06 16:36:30 +0900

    passport update

---
+++ Back/passport/googleStrategy.ts
@@ -0,0 +1,47 @@
+import passport from "passport";
+import GoogleStrategy from 'passport-google-oauth2';
+import User from '../models';
+
+module.exports = () => {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: 'http://ozam.shop/user/google/callback',
+      },
+    //   function (accessToken, refreshToken, profile, cb) {
+    //     User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //       return cb(err, user);
+    //     });
+    //   }
+    async (accessToken, refreshToken, profile, done) => {
+        try {
+          const exUser = await User.findOne({
+            where: { snsId: profile.id, provider: "google" },
+          });
+          if (exUser) {
+            done(null, exUser);
+          } else {
+            console.log(profile.name);
+            const newUser = await User.create({
+              email: profile._json && profile._json.email,
+              nickname: '아무개'+getRandomInt(1,10000),
+              // nickname: profile.displayName,
+              snsId: profile.id,
+              provider: "google",
+            });
+            console.log(newUser);
+            done(null, newUser);
+          }
+        } catch (error) {
+          console.error(error);
+          done(error);
+        }
+      }
+    )
+  );
+};
+public getRandomInt(min, max) { //min ~ max 사이의 임의의 정수 반환
+  return Math.floor(Math.random() * (max - min)) + min;
+}
---
+++ Back/passport/index.ts
@@ -0,0 +1,28 @@
+import passport from "passport";
+// const google = require('passport-google-oauth2');
+import google from './googleStrategy';
+import { User } from '../models';
+import ExtractJwt, Strategy: JWTStrategy from 'passport-jwt';
+import jwt from './jwtStrategy';
+
+
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    console.log(user, '여기어때?')
+    done(null, user.dataValues.userId);
+    console.log(user.dataValues.userId);
+    
+  });
+
+  passport.deserializeUser((id, done) => {
+    console.log(id, '넌 뭐니?')
+    User.findOne({ where: { userId : id } })
+      .then((user) => done(null, user))
+      .catch((err) => done(err));
+  });
+  google();
+  jwt();
+  // local();
+  // kakao();
+};
