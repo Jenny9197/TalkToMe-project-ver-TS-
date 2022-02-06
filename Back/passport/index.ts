bef6b393d3826dda1ca17d2dee1f3d79213ab54a
Jenny9197 <passionnhope0110@gmail.com>
2022-02-06 17:45:17 +0900

    [fix] index.ts from passport

--- Back/passport/index.ts
+++ Back/passport/index.ts
@@ -1,28 +1,24 @@
 import passport from "passport";
-// const google = require('passport-google-oauth2');
-import google from './googleStrategy';
+import *as google from './googleStrategy'; //전체적으로 구글로 치환한다
 import { User } from '../models';
-import ExtractJwt, JWTStrategy from 'passport-jwt';
-import jwt from './jwtStrategy';
+import *as jwt from './jwtStrategy'; // 전체적으로 jwt로 치환한다
 
 
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
     console.log(user, '여기어때?')
-    done(null, user.dataValues.userId);
-    console.log(user.dataValues.userId);
+    done(null, user.userId);
+    console.log(user.userId);
     
   });
 
-  passport.deserializeUser((id, done) => {
+  passport.deserializeUser((id:number, done) => {
     console.log(id, '넌 뭐니?')
     User.findOne({ where: { userId : id } })
       .then((user) => done(null, user))
       .catch((err) => done(err));
   });
-  google();
-  jwt();
-  // local();
-  // kakao();
+  google;
+  jwt;
 };
