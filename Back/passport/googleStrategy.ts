841183bdd55941192b5cecc1a0f91479f449c1bf
leubenia <hdodo777@gmail.com>
2022-02-06 16:47:25 +0900

    [fix] googleStrategy

--- Back/passport/googleStrategy.ts
+++ Back/passport/googleStrategy.ts
@@ -1,13 +1,16 @@
 import passport from "passport";
 import GoogleStrategy from 'passport-google-oauth2';
-import User from '../models';
+import { User } from '../models';
 
+function getRandomInt(min:number, max:number) { //min ~ max 사이의 임의의 정수 반환
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 module.exports = () => {
   passport.use(
-    new GoogleStrategy(
+    new GoogleStrategy.Strategy(
       {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        clientID: process.env.GOOGLE_CLIENT_ID||"test",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET||"test",
         callbackURL: 'http://ozam.shop/user/google/callback',
       },
     //   function (accessToken, refreshToken, profile, cb) {
@@ -26,7 +29,7 @@ module.exports = () => {
             console.log(profile.name);
             const newUser = await User.create({
               email: profile._json && profile._json.email,
-              nickname: '아무개'+getRandomInt(1,10000),
+              nickname: '아무개'+ getRandomInt(1,10000),
               // nickname: profile.displayName,
               snsId: profile.id,
               provider: "google",
@@ -42,6 +45,3 @@ module.exports = () => {
     )
   );
 };
-public getRandomInt(min, max) { //min ~ max 사이의 임의의 정수 반환
-  return Math.floor(Math.random() * (max - min)) + min;
-}
