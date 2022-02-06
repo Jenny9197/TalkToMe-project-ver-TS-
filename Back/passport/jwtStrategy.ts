d04757378db4923723b847f0e754659dda9189d9
Jenny9197 <passionnhope0110@gmail.com>
2022-02-06 17:18:48 +0900

    [modified] jwtStrategy.ts & temporary commentControl

--- Back/passport/jwtStrategy.ts
+++ Back/passport/jwtStrategy.ts
@@ -1,16 +1,16 @@
 import passport = require('passport');
-import passprotjwt from 'passport-jwt';
+import passportjwt from 'passport-jwt';
 import {User} from '../models';
 import { UserModel } from '../models/userModel';
 
 const JWTConfig = {
-  jwtFromRequest: passprotjwt.ExtractJwt.fromAuthHeaderAsBearerToken(),
+  jwtFromRequest: passportjwt.ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.SECRET_KEY,
 };
 
-const JWTVerify = async (jwtPayload:UserModel, done:passprotjwt.VerifiedCallback) => {
+const JWTVerify = async (jwtPayload:UserModel, done:passportjwt.VerifiedCallback) => {
   try {
-      console.log('어디냐?',jwtPayload)
+      console.log('where?',jwtPayload)
       const userid:number = jwtPayload.userId as number;
     const user = await User.findOne({
       where: { userId: userid },
@@ -20,16 +20,16 @@ const JWTVerify = async (jwtPayload:UserModel, done:passprotjwt.VerifiedCallback
       done(null, user);
       return;
     } else {
-      done(null, false, { msg: '존재하지 않는 사용자 입니다.' });
+      done(null, false, { msg: 'Not found as existed user' });
     }
-    done(null, false, { msg: '올바르지 않은 인증정보 입니다.' });
+    done(null, false, { msg: 'Incorrect verified information' });
   } catch (error) {
     console.error(error);
     done(error);
   }
 };
 function jwt() {
-  passport.use('jwt', new passprotjwt.Strategy(JWTConfig, JWTVerify));
+  passport.use('jwt', new passportjwt.Strategy(JWTConfig, JWTVerify));
 }
 
 module.exports = {JWTConfig, JWTVerify, jwt}
